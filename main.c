966ec1dd9ad6eb43ffa8f6db8541d3e0bc1c4d71
rald <thecoconutnutisagiantnut@gmail.com>
2023-03-14 11:46:38 +0800

    OK

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define GRAPHICS_IMPLEMENTATION
 #include "graphics.h"
 
-
+#define GAME_TITLE "Pantasya - C: SDL2 Test"
 
 #define SCREEN_WIDTH 256
 #define SCREEN_HEIGHT 256
@@ -33,13 +33,19 @@ int pxi[P_MAX],pyi[P_MAX];
 int r,g,b;
 int f=0;
 
+
+int sgn(int x) {
+	return x<0?-1:x>0?1:0;
+}
+
+
 int main(void) {
 
 	srand(time(NULL));
 
 	SDL_Init(SDL_INIT_VIDEO);
 
-	window = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+	window = SDL_CreateWindow( GAME_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 
 	renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
@@ -85,12 +91,12 @@ int main(void) {
 			px[i]+=pxi[i];
 			py[i]+=pyi[i];
 
-			if(px[i]<0) { px[i]=0; pxi[i]=rand()%5+1; }
-			if(py[i]<0) { py[i]=0; pyi[i]=rand()%5+1; }
+			if(px[i]<0) { px[i]=0; pxi[i]=abs(pxi[i]); }
+			if(py[i]<0) { py[i]=0; pyi[i]=abs(pyi[i]); }
 
-			if(px[i]>=SCREEN_WIDTH) { px[i]=SCREEN_WIDTH-1; pxi[i]=-(rand()%5+1); }		
+			if(px[i]>=SCREEN_WIDTH) { px[i]=SCREEN_WIDTH-1; pxi[i]=-abs(pxi[i]); }		
 
-			if(py[i]>=SCREEN_HEIGHT) { py[i]=SCREEN_HEIGHT-1; pyi[i]=-(rand()%5+1); }		
+			if(py[i]>=SCREEN_HEIGHT) { py[i]=SCREEN_HEIGHT-1; pyi[i]=-abs(pyi[i]); }		
 
 		}
 
@@ -107,6 +113,10 @@ int main(void) {
 			r=rand()%256;
 			g=rand()%256;
 			b=rand()%256;
+			for(size_t i=0;i<P_MAX;i++) {
+	 			pxi[i]=sgn(pxi[i])*(rand()%5+1);
+	 			pyi[i]=sgn(pyi[i])*(rand()%5+1);
+	 		}
 		}
 
 	}
